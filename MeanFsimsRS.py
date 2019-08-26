685cc1bce0221ab2e6408e158d2e3021b5c35035
Daniel Baker <danieltb@telus.net>
2019-08-26 11:06:54 -0400

    Minor Fixes

--- MeanFsimsRS.py
+++ MeanFsimsRS.py
@@ -42,11 +42,8 @@ parser.add_argument('-f',type=str,help='fname prefix')
 parser.add_argument('-th', type=int,default = 8,help='Number of Threads')
 parser.add_argument('-nf', type=int,default = 6000,help='Number of Channels')
 parser.add_argument('-nt', type=int,default = 2108,help='Number of Time Bins')
-parser.add_argument('-G',dest='G',action='store_true',help='Use Global Fiducial C')
 parser.add_argument('-al',type=float,default=0,help='Global Frequency Scaling')
 parser.add_argument('-samp',type=int,help='MCMC sample Number')
-
-parser.set_defaults(G=False)
 
 args=parser.parse_args()
 fname=args.f+str(args.d)
@@ -56,11 +53,6 @@ if args.al==0:
 	dr='./AL0/'
 else:
 	dr='./AL%s/' % format(args.al,'.3e')
-
-##Import Fit Parameters
-popt=np.load(fname+'Fit.npy')
-N=popt[0]
-popt2=np.load(args.f+'GlobFit.npy')
 
 nt=args.nt
 nf=args.nf
@@ -109,7 +101,7 @@ tarr=tarr.T
 X=(tarr,farr)
 
 MCMC=np.load('%sSamples.npz' %args.f)
-date_idx=np.argwhere(MCMC['dates']==args.d)[0]
+date_idx=np.argwhere(MCMC['dates']=='%sdspec.npy' %args.d)[0]
 glob_fit=np.zeros(13)
 glob_fit[np.array([0,1,2,3,4,5,10])]=MCMC['samps'][:,args.samp,date_idx*7:(date_idx+1)*7].mean(0)
 glob_fit[np.array([6,7,8,9,11,12])]=MCMC['samps'][:,args.samp,-6:].mean(0)
@@ -118,8 +110,8 @@ CC=np.copy(fitmod.gauss_to_chi(CG,fft_G1,fft_G2,fft_object_GF,fft_object_GB))
 N=glob_fit[0]
 
 ##Import Estimator Variables
-A=np.load('%s%sA_%s.npy' % (dr,fname,args.samp))
-K2=np.load('%s%sK2_%s.npy' % (dr,fname,args.samp))
+A=np.load('%s%s/%sA.npy' % (dr,args.samp,fname))
+K2=np.load('%s%s/%sK2.npy' % (dr,args.samp,fname))
 F2=np.zeros(fft_dspec2[1:,:].shape,dtype=complex)
 L=np.zeros(F2.shape)
 for k in range(lengs[int(rank)]):
@@ -153,7 +145,7 @@ comm.Barrier()
 comm.Reduce(L,recvbuff,root=0,op=MPI.SUM)
 if rank==0:
 	recvbuff/=n
-	np.save('%s%sMF_%s.npy' % (dr,fname,args.samp),recvbuff)
+	np.save('%s%s/%sMF.npy' % (dr,args.samp,fname),recvbuff)
 	
 	
 
